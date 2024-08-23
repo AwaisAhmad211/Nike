78f0dedd40f4fa17068a331622182e21265a2d40
Awais Ahmad <itxawais211@gmail.com>
2024-08-23 08:21:49 -0700

    Navbar Completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { headerLogo } from '../assets/images'
+import { navLinks } from '../constants'
+import { hamburger } from '../assets/icons'
+
+
+const Navbar = () => {
+  return (
+    <header className='w-full absolute'>
+        <nav className='flex justify-around py-4'>
+            <a href="/"><img src={headerLogo} alt="Logo" width={100}/></a>
+            <div>
+                <ul className='flex gap-4 flex-row max-md:hidden'>
+                    {navLinks.map((item)=>(
+                        <div>
+                            <a href={item.href}>{item.label}</a>
+                        </div>
+                    ))}
+                </ul>
+            </div>
+            <a className='text-blue-500 underline font-semibold max-md:hidden' href="#">see more</a>
+            <img className='max-md:block hidden' src={hamburger} alt="Hamburger" width={20} height={20}/>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
