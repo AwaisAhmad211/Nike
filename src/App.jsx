ee4f4ca040db4ba2d882e95f984b2222beffd12c
Awais Ahmad <itxawais211@gmail.com>
2024-08-24 20:43:08 -0700

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Hero from './sections/Hero'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
+    <section className='xl:padding-y xl:padding-x'>
      <Hero/>
+    </section>
     </>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import { arrowRight } from '../assets/icons'
 const Button = () => {
   return (
     
-            <button className='sm:mx-16 mx-8 mt-10 bg-coral-red rounded-full border-2 border-coral-red text-white text-sm p-1 flex gap-2'>Shop Now <img src={arrowRight} alt="Arrow" width={18} height={18} /></button>
+            <button className='sm:mx-16 text-xl text-center items-center mx-8 mt-10 bg-coral-red rounded-full border-2 border-coral-red text-white py-3 px-7 flex gap-2'>Shop Now <img src={arrowRight} alt="Arrow" width={18} height={18} className='hover:rotate-[360] duration-500 rotate-180'/></button>
   )
 }
 
